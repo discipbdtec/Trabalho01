f95b77f40c69bbce57bffa09bdc2dd311cf7936b
Matheus Costa e George Matheus <matheus20costa@hotmail.com>
2016-11-12 18:29:42 -0200

    Consultas

---
+++ GAP_Consultas_9.1.sql
@@ -0,0 +1,10 @@
+use gap;
+
+select * from site_academia;
+select * from usuario;
+select * from gerente;
+select * from professor;
+select * from aluno;
+select * from contato;
+select * from formas_de_pagamento;
+select * from acompanha;
