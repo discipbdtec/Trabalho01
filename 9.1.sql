1209a4b0cf295e9c0647a2067fba896a964a1fb0
Matheus Costa e George Matheus <matheus20costa@hotmail.com>
2016-11-29 15:52:24 -0200

    Prints Finais

---
+++ 9.1.sql
@@ -0,0 +1,13 @@
+use gap;
+
+select * from acompanha;
+select * from aluno;
+select * from contato;
+select * from contato_usuario;
+select * from endereco;
+select * from gerente;
+select * from pagamento_aluno;
+select * from professor;
+select * from site_academia;
+select * from tipo_pagamento;
+select * from usuario;
